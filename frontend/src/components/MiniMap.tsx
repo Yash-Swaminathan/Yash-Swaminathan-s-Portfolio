ccda46ef77826023b59bee88428515b532c9ddb6
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-09-20 17:45:01 -0400

    Fixed Naming on maps

--- frontend/src/components/MiniMap.tsx
+++ frontend/src/components/MiniMap.tsx
@@ -32,15 +32,15 @@ const MiniMap: React.FC<MiniMapProps> = ({
   const [isMapLoaded, setIsMapLoaded] = useState(false);
   const { theme } = useTheme();
 
-  // Get tile layer based on theme (inverted: dark map on light theme, blue map on dark theme)
+  // Get tile layer based on theme
   const getTileLayer = useCallback(() => {
     const tileConfigs = {
       light: {
-        url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+        url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
         attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors © <a href="https://carto.com/attributions">CARTO</a>'
       },
       dark: {
-        url: 'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png',
+        url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
         attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors © <a href="https://carto.com/attributions">CARTO</a>'
       }
     };
