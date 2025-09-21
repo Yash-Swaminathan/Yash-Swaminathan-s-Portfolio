83712cbb938bf02c255697bf42afbdfa75197dd5
Yash-Swaminathan <yashswaminathan1@gmail.com>
2025-09-20 22:02:34 -0400

    Refactor App component to remove map visibility toggle and embed map directly in individual pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { MapProvider } from './contexts/MapContext';
 import Navigation from './components/Navigation';
-import CurrentCityMap, { MapToggleButton } from './components/CurrentCityMap';
+// Map is now embedded in individual pages
 import Me from './pages/Me';
 import Projects from './pages/Projects';
 import ProjectDetail from './pages/ProjectDetail';
 
 function App() {
-  const [isMapVisible, setIsMapVisible] = useState(true);
-
   return (
     <ThemeProvider>
       <MapProvider>
@@ -22,22 +20,6 @@ function App() {
               <Route path="/projects" element={<Projects />} />
               <Route path="/projects/:slug" element={<ProjectDetail />} />
             </Routes>
-
-            {/* Mini Map */}
-            {isMapVisible && (
-              <CurrentCityMap
-                width={280}
-                height={200}
-                top="80px"
-                right="20px"
-              />
-            )}
-
-            {/* Map Toggle Button */}
-            <MapToggleButton
-              onToggle={() => setIsMapVisible(!isMapVisible)}
-              isVisible={isMapVisible}
-            />
           </div>
         </Router>
       </MapProvider>
